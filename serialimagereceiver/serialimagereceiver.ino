794e3e8e7e428262f1c660d4a5b4c17533daa4c1
Raymond Blum <raymond@Raymonds-MacBook.local>
2016-12-24 17:03:19 -0500

    debugging code added - needs work - memory overwrites are clobbering framecounter

--- serialimagereceiver/serialimagereceiver.ino
+++ serialimagereceiver/serialimagereceiver.ino
@@ -1,11 +1,14 @@
 #include <FastLED.h>
 
+#define PANEL_HEIGHT 8
+#define PANEL_WIDTH 32
+
 #define HWSERIAL Serial2
 #define LEDPIN 13
 int pixels;
 bool recvd;
-#define ROWS 16
-#define COLUMNS 32
+const int ROWS = PANEL_HEIGHT * 2;
+const int  COLUMNS = PANEL_WIDTH;
 
 #define NUM_LEDS (COLUMNS * ROWS)
 CRGB leds_plus_safety_pixel[ NUM_LEDS];
@@ -14,14 +17,14 @@ CRGB* const leds( leds_plus_safety_pixel);
 const byte FRAME_HEADER_1 = 0xF0;
 const byte FRAME_HEADER_2 = 0x00;
 const byte FRAME_HEADER_3 = 0x0F;
-int frame;
+unsigned long frame;
 int row;
 
 
 #define LED_PIN  3
 #define COLOR_ORDER RGB
 #define CHIPSET     WS2811
-#define BRIGHTNESS 64
+#define BRIGHTNESS 32
 
 void setup() {
   Serial.begin(9600);
@@ -59,19 +62,73 @@ bool getNextByte(byte *b) {
 void getFrame() {
   pixels = 0;
   while (row < ROWS) {
-    byte b;
     byte colors[3];
-    for (int colorIdx = 0; colorIdx < 3; colorIdx++)
-      while (!getNextByte(&(colors[colorIdx]))) ;
+    /*
+    Serial.print("getting pixel r=");
+    Serial.print(row);
+    Serial.print(",c=");
+    Serial.print(pixels);
+    */
+    for (int colorIdx = 0; colorIdx < 3; colorIdx++) {
+      while (!getNextByte(&(colors[colorIdx])));
+      //Serial.print(".");
+    }
+    //Serial.println();
     pixels += 1;
-    leds[row*COLUMNS+pixels] = CRGB( colors[0], colors[1], colors[2]);
+    leds[getPixelForCoord(pixels,row)] = CRGB( colors[0], colors[1], colors[2]);
     if (!(pixels % COLUMNS)) {
       row += 1;
+      pixels = 0;
     }
   }
   Serial.print("END OF FRAME: ");
   Serial.println(frame++);
+  if (frame > 1000) {
+    Serial.println("Frame counter reset");
+    frame = 0;
+  }
   row = 0;
+}
+
+int getPixelForCoord(int x, int y) {
+  if (y < PANEL_HEIGHT)
+    return coordForPanel1(x, y);
+  else
+    return coordForPanel2(x, y);
+}
+
+int coordForPanel1(int x, int y) {
+  if (x % 2)
+    return countUpPanel1(x, y);
+  else
+    return countDownPanel1(x, y);
+}
+  
+int coordForPanel2(int x, int y) {
+  if (x % 2)
+    return countUpPanel2(x, y);
+  else
+    return countDownPanel2(x, y);
+}
+
+int countDownPanel1(int x, int y) {
+  return x * PANEL_HEIGHT + y;
+}
+
+int countUpPanel1(int x, int y) {
+  return x * PANEL_HEIGHT + (PANEL_HEIGHT - 1 - y);
+}
+
+int countDownPanel2(int x, int y) {
+  return (PANEL_WIDTH * PANEL_HEIGHT - 1)
+    - (PANEL_HEIGHT * (x+1))
+    + (y - PANEL_HEIGHT);
+}
+
+int countUpPanel2(int x, int y) {
+  return ((PANEL_HEIGHT * PANEL_WIDTH) - 1)
+    + (x * PANEL_HEIGHT)
+    - (y - PANEL_HEIGHT);
 }
 
 bool syncToFrame() {
@@ -86,12 +143,13 @@ bool syncToFrame() {
   if (b != FRAME_HEADER_3) {
     return false;
   }
+  Serial.println("sync");
   return true;
 }
 
 void loop() {
   digitalWrite(LEDPIN, LOW);
-  while (!syncToFrame()) ;
+  while (!syncToFrame()) Serial.println("Failed to Sync");
   digitalWrite(LEDPIN, HIGH);
   getFrame();
   digitalWrite(LEDPIN, LOW);
