fe80df9e5d6ce4d1d6cea5135ea77fe62b2af7e1
raygeeknyc <raygeeknyc@gmail.com>
2016-12-30 01:32:55 -0500

    Added footer for frame padding due to lack of flow cotrol Fixed image resizing source better debugging added option for equalizing brighness

--- imageprocessor.py
+++ imageprocessor.py
@@ -1,14 +1,22 @@
 from picamera import PiCamera
 from picamera.array import PiRGBArray
 import cv2
+import numpy
 import serial
+import sys
 import time
 
 _DEBUG = False
 #_DEBUG = True
 
+equalize = False
+if len(sys.argv) > 1 and sys.argv[1] == "equalize":
+	equalize = True
+
 if _DEBUG:
 	print "Starting image processor"
+	if equalize:
+		print "Equalizing brightness"
 frameTimerDuration = 1
 
 # The desired resolution of the Pi camera
@@ -30,14 +38,17 @@ framerate = 0
 displayWidth = 32.0
 displayHeight = 16.0
 
+FOOTER_LENGTH = 330
+
 # These are the horizontal margins of the input feed to crop, everything else scales to fit these
 xLeft = 0
 xRight = 0
 
 # These are the bytes of our frame header, used to sync frame boundaries on the receiver
-FRAME_HEADER_1 = chr(int('0xf0',16))
-FRAME_HEADER_2 = chr(int('0x00',16))
-FRAME_HEADER_3 = chr(int('0x0f',16))
+FRAME_HEADER_1 = chr(int('0x01',16))
+FRAME_HEADER_2 = chr(int('0x02',16))
+FRAME_HEADER_3 = chr(int('0x03',16))
+FRAME_FOOTER = chr(int('0x00',16))
 
 # Open serial port at the highest tested standard BAUD rate
 ser = serial.Serial(SERPORT, BAUD, timeout=1)
@@ -55,15 +66,16 @@ print "input resolution is %d,%d" % resolution
 print "target resolution is %d,%d" % (displayWidth, displayHeight)
 
 def equalize_hist(img):
+    equalized = numpy.copy(img)
     for c in xrange(0, 2):
-       img[:,:,c] = cv2.equalizeHist(img[:,:,c])
-    return img
+       equalized[:,:,c] = cv2.equalizeHist(equalized[:,:,c])
+    return equalized
 
 def equalize_brightness(img):
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     hsv[:,:,2] = cv2.equalizeHist(hsv[:,:,2])
-    img = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
-    return img
+    bgr = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+    return bgr
 
 if _DEBUG:
 	cv2.namedWindow("Original")
@@ -83,9 +95,8 @@ print "min = %d, max = %d, height = %d" % (_yMin, _yMax, _height)
 downsampleXFactor = displayWidth / _width
 downsampleYFactor = displayHeight / _height
 print "Crop to (%d,%d)=%d:(%d,%d)=%d" % (_xMin,_xMax,(_xMax-_xMin),_yMin,_yMax,(_yMax-_yMin))
-if _DEBUG:
-	print "Scaling by (x,y) %f, %f" % (downsampleXFactor, downsampleYFactor)
-	print "Scales to (%d,%d)" % (_width*downsampleXFactor,_height*downsampleYFactor)
+print "Scaling by (x,y) %f, %f" % (downsampleXFactor, downsampleYFactor)
+print "Scales to (%d,%d)" % (_width*downsampleXFactor,_height*downsampleYFactor)
 
 def writeFrameHeader():
 	written = ser.write(FRAME_HEADER_1)
@@ -100,6 +111,14 @@ def writeFrameHeader():
 	if written != 1:
 		print "failed to send header 3"
 		return False
+	return True
+
+def writeFrameFooter():
+	for i in range(FOOTER_LENGTH):
+		written = ser.write(FRAME_FOOTER)
+		if written != 1:
+			print "failed to send footer"
+			return False
 	return True
 
 def writeFrame(image):
@@ -130,11 +149,14 @@ try:
                         frameCounter = 0
                         frameTimer = time.time() + frameTimerDuration
                 cropImg = img[_yMin:_yMax,_xMin:_xMax] # this is all there is to cropping
-                small = cv2.resize(img, (0,0), fx=downsampleXFactor, fy=downsampleYFactor)
-		small = equalize_brightness(small)
-
+		if equalize:
+			cropImg = equalize_brightness(cropImg)
+                small = cv2.resize(cropImg, (0,0), fx=downsampleXFactor, fy=downsampleYFactor)
+		if _DEBUG:
+			print "derez to {y=%d,x=%d}" % (len(small),len(small[0]))
 		writeFrameHeader();
 		writeFrame(small);
+		writeFrameFooter();
 
 		if _DEBUG:
 			cv2.imshow("Original", img)
