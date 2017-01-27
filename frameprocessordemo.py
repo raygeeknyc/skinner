f5d8f275d1f517a07f648e1716379af5a5b6af56
Raymond Blum <rsygeeknyc@gmail.com>
2017-01-27 13:26:41 -0500

    Propagated fix for aspect ratio of cropped images to the demo.

--- frameprocessordemo.py
+++ frameprocessordemo.py
@@ -35,8 +35,8 @@ _displayAspectRatio = displayHeight / displayWidth
 print "aspect ratio %f" % _displayAspectRatio
 _xMin = xLeft
 _xMax = resolution[0]-xRight
-_height = int(_displayAspectRatio * resolution[0])
 _width = _xMax+1 - _xMin
+_height = int(_displayAspectRatio * width)
 _yMin = int((resolution[1] - _height)/2)
 _yMax = _yMin + _height
 print "min = %d, max = %d, height = %d" % (_yMin, _yMax, _height)
