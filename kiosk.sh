ad36e799fecd0149fe14cd40fdfabf058f22df15
Raymond 'Princess Sparklefists' Blum <raymondb@raymondb-macbookpro2.roam.corp.google.com>
2017-02-16 09:24:59 -0500

    ADded logging

--- kiosk.sh
+++ kiosk.sh
@@ -4,7 +4,11 @@ cd /home/raymond/Documents/workspace/skinner
 git pull
 rc=$?
 if [[ $rc -eq 0 ]]; then
+  logger "$0 pulled new code"
   echo "pulled new code"
+else
+  logger "$0 Error pulling new code"
+  echo "Error pulling new code"
 fi
 cat /dev/null > nohup.out
 nohup python imageprocessor.py
